1f2b1042c2dd380e721916891e8cbecb3d3cd400
erik savage <erik.k.savage@gmail.com>
2022-01-10 15:47:58 -0800

    adds express routes, adds tests

---
+++ app.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const express = require('express');
+
+// executing express express() is a singleton pattern that returns an object that can be modified.
+const app = express();
+
+const messages = [];
+
+class Message {
+    constructor(text, author) {
+        this.text = text;
+        this.author = author;
+    }
+}
+
+//requires 2 things, route (string), Callback function (Request Response)
+app.get('/message', (request, response) => {
+    console.log(`Someone sent a request!: ${request.method}`);
+
+    response.send(messages);
+    //create a message and return it
+}); //this method/function modifies our app single ton
+
+function createMessage(req, res, next) {
+    const messageText = req.query.text;
+    const authorName = req.query.author;
+
+    console.log('first message created')
+
+    const message = new Message(messageText, authorName);
+
+    req.message = message;
+    next();
+}
+
+function saveMessage(req, res, next) {
+    console.log('we can see any data added to the request', req.message);
+    let message = req.message;
+    messages.push(message);
+    next();
+}
+
+
+// POST: http://localhost3000/message?text=somestring&author=somename
+app.post('/message', createMessage, saveMessage, (request, response, next) => {
+    response.send(messages);
+});
+
+
+app.use(function (err, request, response, next) {
+    response.send('error handler hit!');
+});
+
+app.use(function (request, response) {
+    response.status(404).send('Nothing Found!');
+});
+
+module.exports = {
+    start: function (port) {
+        app.listen(port, () => {
+            console.log(`App is running on : ${port}`);
+        });
+    },
+    app,
+};
+
+// module.exports = app;
