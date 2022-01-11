d23a05d459a29787fde83d385fa3f68d14d00cdc
erik savage <erik.k.savage@gmail.com>
2022-01-10 17:25:56 -0800

    updates indentation, changes test and start scripts

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -5,10 +5,10 @@ const supertest = require('supertest');
 const request = supertest(server.app);
 
 describe('Testing my HTTP server', () => {
-    it('should be able to response to a POST to /message', async () => {
-        let response = await request.post('/message?text=test&author=test');
+  it('should be able to response to a POST to /message', async () => {
+    let response = await request.post('/message?text=test&author=test');
 
-        expect(response.status).toEqual(200);
-        expect(response.body[0].text).toEqual('test');
-    });
-})
+    expect(response.status).toEqual(200);
+    expect(response.body[0].text).toEqual('test');
+  });
+});
